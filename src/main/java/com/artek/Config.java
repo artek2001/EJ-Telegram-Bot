2b508dcd21faf8c12120951ddd59e7252e5971a3
Artek <artek2001@gmail.com>
2018-12-01 20:08:17 +0300

    Update Config.java

--- src/main/java/com/artek/Config.java
+++ src/main/java/com/artek/Config.java
@@ -1,6 +1,6 @@
 package com.artek;
 
 public class Config {
-    public static final String TOKEN= "735016648:AAF4Y50sOv4Y9wwSo3_MSQhkAZe84rIdKpU";
-    public static final String BOTUSERNAME = "ejgrsmu_bot";
+    public static final String TOKEN= "info";
+    public static final String BOTUSERNAME = "info";
 }
